541cab11f54b134e9c98091a423331f65d4d82cb
Abhinav Gangadharan <144839406+abhinavg-neu@users.noreply.github.com>
2025-05-06 12:48:05 -0400

    progress in adding a prompt

--- list.c
+++ list.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define H 5863588
+// #include <editline/readline.h>
 
 typedef struct node {
   int value;
@@ -7,19 +9,27 @@ typedef struct node {
 } Node;
 
 // function definitions
+void initiate_prompt();
 void print_list(Node *head);
+unsigned long strHash(const char *str);
 Node *insert_node_at_head(Node *head, int new_value);
 Node *insert_at_tail(Node *head, int value);
 
+Node *head = NULL;
+static char input[2048];
+int a = 1;
 int main() {
 
-  Node *head = NULL;
+  /*
+   * Starting the prompt
+   */
+  initiate_prompt();
   //  head = insert_node_at_head(head, 7);
   // head = insert_node_at_head(head, 5);
   // head = insert_node_at_head(head, 3);
-  head = insert_at_tail(head, 7);
-  head = insert_at_tail(head, 1);
-  head = insert_at_tail(head, 2);
+  //  head = insert_at_tail(head, 7);
+  // head = insert_at_tail(head, 1);
+  // head = insert_at_tail(head, 2);
   //  Node a, b, c;
   // a.value = 5;
   // b.value = 6;
@@ -29,7 +39,7 @@ int main() {
   // b.next = &c;
   // c.next = NULL;
 
-  print_list(head);
+  //  print_list(head);
 }
 
 Node *insert_node_at_head(Node *head, int new_value) {
@@ -70,4 +80,35 @@ void print_list(Node *head) {
     i++;
     current = current->next;
   }
+}
+
+void initiate_prompt() {
+
+  printf("Welcome to Linked List in C demonstration. type \"h\" or \"help\" "
+         "for help\n");
+  while (1) {
+    printf("Enter Your command> ");
+    scanf("%2047s", input);
+    // SWITCH STATEMENT
+    switch (strHash(input)) {
+    case H:
+      printf("Statement 1");
+      break;
+
+    default:
+      printf("Not a valid entry\n");
+      printf(" Type \"h\" for help\n");
+      break;
+    }
+  }
+}
+
+unsigned long strHash(const char *str) {
+  unsigned long hash = 5381;
+  int c;
+
+  printf("str is : %s", str);
+  while ((c = *str++))
+    hash = ((hash << 5) + hash) + c;
+  return hash;
 }
